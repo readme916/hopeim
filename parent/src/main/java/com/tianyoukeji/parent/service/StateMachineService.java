99559555f77943ddbe2836d087baa1861cfbdf05
readme916 <1290144599@qq.com>
2020-01-23 21:06:29 +0800

    1

--- parent/src/main/java/com/tianyoukeji/parent/service/StateMachineService.java
+++ parent/src/main/java/com/tianyoukeji/parent/service/StateMachineService.java
@@ -10,13 +10,18 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ResolvableType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.StateMachinePersist;
 import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.StateMachineBuilder;
 import org.springframework.statemachine.config.StateMachineBuilder.Builder;
 import org.springframework.statemachine.config.configurers.ExternalTransitionConfigurer;
 import org.springframework.statemachine.config.configurers.InternalTransitionConfigurer;
+import org.springframework.statemachine.data.redis.RedisStateMachineContextRepository;
+import org.springframework.statemachine.data.redis.RedisStateMachinePersister;
 import org.springframework.statemachine.guard.Guard;
+import org.springframework.statemachine.persist.RepositoryStateMachinePersist;
 import org.springframework.util.StringUtils;
 
 import com.liyang.jpa.smart.query.db.SmartQuery;
@@ -70,9 +75,9 @@ public abstract class StateMachineService<T extends IStateMachineEntity> extends
 			}
 			if(state.getIsChoice()) {
 				builder.configureStates().withStates().choice(state.getCode());
-				builder.configureTransitions().withChoice().first(state.getFirstTarget().getCode(), guardSpel(state.getFirstGuardSpel()));
+				builder.configureTransitions().withChoice().first(state.getFirstTarget().getCode(), guardFactory(state.getFirstGuardSpel()));
 				if(state.getThenTarget()!=null) {
-					builder.configureTransitions().withChoice().then(state.getThenTarget().getCode(), guardSpel(state.getThenGuardSpel()));
+					builder.configureTransitions().withChoice().then(state.getThenTarget().getCode(), guardFactory(state.getThenGuardSpel()));
 				}
 				builder.configureTransitions().withChoice().last(state.getLastTarget().getCode());
 			}
@@ -85,10 +90,33 @@ public abstract class StateMachineService<T extends IStateMachineEntity> extends
 				builder.configureStates().withStates().state(state.getCode(), null, ActionFactory(state.getExitAction()));
 			}
 			
-			
 			Set<Timer> timers = state.getTimers();
 			for (Timer timer : timers) {
-				
+				if(timer.getTarget()==null) {
+					 InternalTransitionConfigurer<String, String> eventTemp1 = builder.configureTransitions().withInternal().source(state.getCode()).event(timer.getCode());
+					 
+					 if(timer.getTimerInterval()!=null) {
+						 eventTemp1.timer(timer.getTimerInterval());
+					 }
+					 if(timer.getTimerOnce()!=null) {
+						 eventTemp1.timerOnce(timer.getTimerOnce());
+					 }
+					 if(StringUtils.hasText(timer.getAction())) {
+						 eventTemp1.action(ActionFactory(timer.getAction()));
+					 }
+					 
+				}else {
+					ExternalTransitionConfigurer<String, String> eventTemp2 = builder.configureTransitions().withExternal().source(state.getCode()).target(timer.getTarget().getCode());
+					 if(timer.getTimerInterval()!=null) {
+						 eventTemp2.timer(timer.getTimerInterval());
+					 }
+					 if(timer.getTimerOnce()!=null) {
+						 eventTemp2.timerOnce(timer.getTimerOnce());
+					 }
+					if(StringUtils.hasText(timer.getAction())) {
+						 eventTemp2.action(ActionFactory(timer.getAction()));
+					 }
+				}
 			}
 			
 			Set<Event> events = state.getEvents();
@@ -114,19 +142,32 @@ public abstract class StateMachineService<T extends IStateMachineEntity> extends
 			}
 		}
 		
-		
+		System.out.println(structure.getName()+"状态机初始化完成");
 	}
 	
-	public Action<String,String> ActionFactory(String actionStr){
+	@Bean
+	public StateMachinePersist<String, String, String> stateMachinePersist(RedisConnectionFactory connectionFactory) {
+		RedisStateMachineContextRepository<String, String> repository =
+				new RedisStateMachineContextRepository<String, String>(connectionFactory);
+		return new RepositoryStateMachinePersist<String, String>(repository);
+	}
+
+	@Bean
+	public RedisStateMachinePersister<States, Events> redisStateMachinePersister(
+			StateMachinePersist<States, Events, String> stateMachinePersist) {
+		return new RedisStateMachinePersister<States, Events>(stateMachinePersist);
+	}
+	
+	private Action<String,String> ActionFactory(String actionStr){
 		return new Action<String, String>() {
 			@Override
 			public void execute(StateContext<String, String> context) {
-				
+				System.out.println(actionStr);
 			}
 		};
 	}
 	
-	public Guard<String, String> guardFactory(String spel) {
+	private Guard<String, String> guardFactory(String spel) {
 		return new Guard<String, String>() {
 			@Override
 			public boolean evaluate(StateContext<String, String> context) {
